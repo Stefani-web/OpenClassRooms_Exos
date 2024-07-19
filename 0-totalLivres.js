c4be17ceb73c94cb2ede7397efb5deb577832cdf
Stefani-web <stephaniecvlho@gmail.com>
2024-07-19 12:19:00 +0200

    Declare a number variable

--- 0-totalLivres.js
+++ 0-totalLivres.js
@@ -4,9 +4,3 @@ totalLivres -= 10
 totalLivres += 5
 
 console.log(totalLivres)
-
-let affichageTotalLivres = "Notre bibliothéque possède"
-affichageTotalLivres += totalLivres
-affichageTotalLivres += " livres "
-
-console.log(affichageTotalLivres)
