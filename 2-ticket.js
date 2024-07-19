cb00f341efaf47bbe4eb9d5154a510157c15461a
Stefani-web <stephaniecvlho@gmail.com>
2024-07-19 16:16:21 +0200

    Declare objects

---
+++ 2-ticket.js
@@ -0,0 +1,14 @@
+const ticket = {
+  nomFilm: "Retour vers le futur",
+  prix: 12,
+  numeroSalle: 7,
+}
+
+let nom = "Stefani"
+
+console.log(ticket)
+console.log(nom)
+
+const texteAffichage = "Bonjour Stefani, votre film Retour vers le futur est en salle 7"
+
+console.log(texteAffichage)
