4b703fd0352cf05542996730491428ecf73847dd
Stefani-web <stephaniecvlho@gmail.com>
2024-07-22 11:52:51 +0200

    Group into Fistrpart file

---
+++ FirstPart/1-add_affichage_total_livres.js
@@ -0,0 +1,12 @@
+let totalLivres = 500
+totalLivres += 50
+totalLivres -= 10
+totalLivres += 5
+
+console.log(totalLivres)
+
+let affichageTotalLivres = "Notre bibliothéque possède "
+affichageTotalLivres += totalLivres
+affichageTotalLivres += " livres "
+
+console.log(affichageTotalLivres)
---
+++ FirstPart/5-delete_last_song.js
@@ -0,0 +1,8 @@
+let playlist = ["Fix you", "Westside", "Always Remember Us This Way"]
+
+playlist.push("New Flame")
+playlist.push("Residuals")
+
+playlist.pop()
+
+console.log(playlist)
